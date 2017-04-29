75fd7d0e929d4367000c56c97b8a8e8d7b9673b6
John Krowas <jkrowas@gmail.com>
2017-04-28 19:11:33 -0700

    added grant input file reader, setup, requirements

--- fauto.py
+++ fauto.py
@@ -2,13 +2,13 @@ import splinter as sp
 import time
 from pywinauto import Application
 import logging
+import sys
+import csv
 
-
-#logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
@@ -17,9 +17,65 @@ prod_landing_url = "https://chanzuckerberg.fluxx.io/user_sessions/new"
 username = 'nina@chanzuckerberg.com'
 password = 'Welcome!'
 grant_id = 'HCA-A-1703-00264'
-upload_file = '00264.pdf'
+upload_file = "c:\\temp\\OISP_July.pdf"
 
 url = test_landing_url
+
+
+def get_grants_to_process(filename):
+    grants = []
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            grants.append(row[0])
+    return grants
+
+
+def do_upload(browser, apps_card, grant_id, upload_file):
+    logger.debug("Beginning upload of %s for grant %s",  upload_file, grant_id)
+    time.sleep(2)
+    logger.info("Finding Application %s", grant_id)
+    myapp = apps_card.find_by_text(grant_id)
+    if len(myapp) > 0:
+        logger.info("Found Application %s", grant_id)
+    else:
+        logger.info("Couldn't find Application %s!", grant_id)
+        return(1)
+    myapp.click()
+    time.sleep(2)
+    try:
+        if browser.is_element_present_by_text("\nInternal Documents\n", wait_time=5):
+            id = browser.find_by_text("\nInternal Documents\n")
+            time.sleep(2)
+            i = id.find_by_tag('i')
+            for j in i:
+                if j.has_class('futil u-cplus-f'):  # Add Document button
+                    print(j.value)
+                    j.click()
+                    if browser.is_element_present_by_text('Add files', wait_time=5):
+                        browser.find_by_text('Add files').click()
+                        time.sleep(1)
+                        app = Application().connect(title="Open")
+                        logger.info("Uploading document %s", upload_file)
+                        app.Open.Edit.set_edit_text(upload_file)
+                        app.Open.Open.close_click()
+                        browser.select("plupload_file_type", "3417")  # 3417 is "Other Document"
+                        browser.find_by_text('Start upload').click()
+                        browser.is_text_present('Upload Complete!', wait_time=60)
+                        logger.info("Upload complete")
+                        browser.find_by_text('Close').click()
+                        return 0
+                    else:
+                        logger.info("Can't find Add Files button")
+                        return 1
+        else:
+            logger.warning("Couldn't find Internal Documents Section")
+            return 1
+    except:
+        logger.error("Unexpected error: %s while processing grant %s", str(sys.exc_info()[0]), grant_id)
+        return 0
+
+
 with sp.Browser('chrome') as browser:
     # Visit URL
     browser.visit(url)
@@ -32,50 +88,41 @@ with sp.Browser('chrome') as browser:
         if browser.is_text_present('Invalid Credentials'):
             logger.fatal("Login failed; invalid credentials")
         else:
-            logger.info("Login Worked!")
+            logger.info("Login successful!")
 
     except:
         logger.fatal("Can't login on page %s" , url)
 
     browser.is_text_present('Applications', wait_time=5)
-    apps_card = browser.find_by_text('Applications').first
-    # # Look for an open application
-    # i = browser.find_by_tag("i")
-    # for j in i:
-    #     if j.has_class("futil u-arrowl-f"):
-    #         j.click()
-    logger.info("Finding Application %s", grant_id)
-    myapp = apps_card.find_by_text(grant_id)
-    if len(myapp) > 0:
-        logger.info("Found Application %s!", grant_id)
-    myapp.click()
-    #wf = apps_card.find_by_text('Workflow')
-    #wf.click()
-    #note = browser.find_by_tag('textarea').first
-    #time.sleep(5)
-    #if note.has_class('optional-workflow-note'):
-    #    note.fill("Foo")
-    time.sleep(2)
-    if browser.is_element_present_by_text("\nInternal Documents\n", wait_time = 5):
-        id = browser.find_by_text("\nInternal Documents\n")
-        i = id.find_by_tag('i')
-        print (len(i))
-        for j in i:
-            if j.has_class('futil u-cplus-f'):
-                print (j.value)
-                j.click()
-                if browser.is_element_present_by_text('Add files', wait_time = 5):
-                    browser.find_by_text('Add files').click()
-                    time.sleep(1)
-                    app = Application().connect(title="Open")
-                    logger.info("Uploading document %s", upload_file)
-                    app.Open.Edit.set_edit_text(upload_file)
-                    app.Open.Cancel.close_click()
-                    browser.find_by_text('Close').click()
-                else:
-                    logger.info("Can't find Add Files button")
+    apps_card = None
+    apps = browser.find_by_text('Applications')
+    for a in apps:
+        if a.has_class('card_name title'):
+            apps_card = a
+    if apps_card is not None:
+        logger.info("Found Applications card")
     else:
-        logger.info("Couldn't find Internal Documents Section")
+        logger.info("Couldn't find Applications card, exiting")
+        exit()
+
+    grants = get_grants_to_process("c:\\users\\kr9\\PycharmProjects\\fauto\\grants.csv")
+    logger.info("Doing %i grants", len(grants))
+    success_count = 0
+    unprocessed = []
+    for g in grants:
+        upload_file = "c:\\temp\\" + g + ".pdf"
+        if do_upload(browser, apps_card, g, upload_file) != 0:
+            unprocessed.append(str(g))
+            logger.warning("Couldn't complete upload for %s", g)
+        else:
+            success_count += 1
+
+    logger.info("Processed %i/%i grants successfully", success_count, len(grants))
+    if len(unprocessed) > 0:
+        logger.warning("These applications were not processed: %s", ', '.join(unprocessed))
+
+
+
 
 
 
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fauto',
+    version='0.9',
+    packages=[''],
+    url='',
+    license='',
+    author='kr9',
+    author_email='',
+    description=''
+)
